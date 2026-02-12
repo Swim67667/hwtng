a2c012378ff2e89384bc0471335bea3e8a588fb4
Swim67667 <150874399+Swim67667@users.noreply.github.com>
2026-02-12 08:47:29 -0500

    Update draw.py

--- draw.py
+++ draw.py
@@ -40,7 +40,7 @@ def predict_digit(sketchpad_data):
     
     # 8. Return as a dictionary for gr.Label
     class_names = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    return {class_names[i]: float(probabilities[i]) for i in range(10)}
+    return {class_names[i]: float(probabilities[i]) for i in range(len(class_names)}
 
 # Launch the interface
 interface = gr.Interface(
